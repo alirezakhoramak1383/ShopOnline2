b5c250e94b6185eeaa83f035f721739535f0b4fe
Amin <akbariamin01@gmail.com>
2024-12-26 13:01:25 +0330

    Add Interface UserServise and Class Dto

---
+++ Shop2.Appliction/Servises/Users/Queries/GetUsers/IGetUserServise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop2.Appliction.Servises.Users.Queries.GetUsers
+{
+    public interface IGetUserServise
+    {
+    }
+    public class GetUsersDto
+    {
+
+    }
+}
