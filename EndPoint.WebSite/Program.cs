314f24e893beef4ebe6cacaf9731be74c596db9c
alireza khorammak <alirezakhorammak@gmail.com>
2024-12-26 21:14:31 +0330

    Create Area Admin

--- EndPoint.WebSite/Program.cs
+++ EndPoint.WebSite/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Shop2.Appliction.Interfaces.Context;
+using Shop2.Appliction.Servises.Users.Queries.GetUsers;
 using Shop2.Persistence.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<IShopContext, ShopContext>();
+builder.Services.AddScoped<IGetUserServise, GetUserService>();
 builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineShop")));
 
 var app = builder.Build();
@@ -27,8 +29,17 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+
+app.MapControllerRoute(
+  name: "areas",
+  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
